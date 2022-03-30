8d12372f1674620b8d409872d99fea9ae29fd1e3
marlena91 <marlena.zarczynska@gmail.com>
2022-03-30 14:34:16 -0700

    Created seeder for Patients Table

---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+use App\Models\Patient;
+use Faker\Generator as Faker;
+
+
+class PatientFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Patient::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'pesel' => $this->faker->numerify('##########'),
+            'name' => $this->faker->firstName($gender = null),
+            'lastname' => $this->faker->lastName,
+            'birthday' => $this->faker->dateTimeBetween($startDate = '-80 years', $endDate = 'now', $timezone = null),
+        ];
+    }
+}
