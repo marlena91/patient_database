1694a862b3b3acbbad1f765810cd5d30704b499a
Marlena Żarczyńska <marlena.zarczynska@gmail.com>
2022-04-01 08:56:18 +0200

    Binded data to search engine

--- public/js/app.js
+++ public/js/app.js
@@ -5445,6 +5445,55 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/vue/components/Search.vue?vue&type=script&lang=js&":
+/*!*****************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/vue/components/Search.vue?vue&type=script&lang=js& ***!
+  \*****************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  components: {},
+  data: function data() {
+    return {
+      name: null,
+      lastname: null,
+      pesel: null,
+      birthday: null
+    };
+  },
+  computed: {},
+  methods: {},
+  created: function created() {},
+  props: {}
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/vue/components/SinglePatient.vue?vue&type=script&lang=js&":
 /*!************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/vue/components/SinglePatient.vue?vue&type=script&lang=js& ***!
@@ -28652,15 +28701,17 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _Search_vue_vue_type_template_id_7d72870a___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Search.vue?vue&type=template&id=7d72870a& */ "./resources/js/vue/components/Search.vue?vue&type=template&id=7d72870a&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _Search_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Search.vue?vue&type=script&lang=js& */ "./resources/js/vue/components/Search.vue?vue&type=script&lang=js&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 ;
-var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _Search_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _Search_vue_vue_type_template_id_7d72870a___WEBPACK_IMPORTED_MODULE_0__.render,
   _Search_vue_vue_type_template_id_7d72870a___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
   false,
@@ -28777,6 +28828,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Patients_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Patients.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/vue/components/Patients.vue?vue&type=script&lang=js&");
  /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Patients_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/vue/components/Search.vue?vue&type=script&lang=js&":
+/*!*************************************************************************!*\
+  !*** ./resources/js/vue/components/Search.vue?vue&type=script&lang=js& ***!
+  \*************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Search_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Search.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/vue/components/Search.vue?vue&type=script&lang=js&");
+ /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Search_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
@@ -29218,62 +29285,121 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
-}
-var staticRenderFns = [
-  function () {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", [
-      _c(
-        "h5",
-        {
-          staticClass: "text-uppercase text-secondary font-weight-bolder mt-3",
-        },
-        [_vm._v("Wyszukiwarka:")]
-      ),
-      _vm._v(" "),
-      _c("div", { staticClass: "form-row mb-4" }, [
-        _c("div", { staticClass: "form-group col-md-6 mt-3" }, [
-          _c("input", {
-            staticClass: "form-control form-control-sm",
-            attrs: { type: "text", name: "from", placeholder: "Imię..." },
-          }),
-        ]),
-        _vm._v(" "),
-        _c("div", { staticClass: "form-group col-md-6 mt-3" }, [
-          _c("input", {
-            staticClass: "form-control form-control-sm",
-            attrs: { type: "text", name: "from", placeholder: "Nazwisko..." },
-          }),
-        ]),
-        _vm._v(" "),
-        _c("div", { staticClass: "form-group col-md-6 mt-3" }, [
-          _c("input", {
-            staticClass: "form-control form-control-sm",
-            attrs: { type: "text", name: "from", placeholder: "Pesel..." },
-          }),
-        ]),
-        _vm._v(" "),
-        _c("div", { staticClass: "form-group col-md-6 mt-3" }, [
-          _c("input", {
-            staticClass: "form-control form-control-sm",
-            attrs: {
-              type: "text",
-              name: "from",
-              placeholder: "Data urodzenia...",
+  return _c("div", [
+    _c(
+      "h5",
+      { staticClass: "text-uppercase text-secondary font-weight-bolder mt-3" },
+      [_vm._v("Wyszukiwarka:")]
+    ),
+    _vm._v(" "),
+    _c("div", { staticClass: "form-row mb-4" }, [
+      _c("div", { staticClass: "form-group col-md-6 mt-3" }, [
+        _c("input", {
+          directives: [
+            {
+              name: "model",
+              rawName: "v-model",
+              value: _vm.name,
+              expression: "name",
             },
-          }),
-        ]),
+          ],
+          staticClass: "form-control form-control-sm",
+          attrs: { type: "text", name: "name", placeholder: "Imię..." },
+          domProps: { value: _vm.name },
+          on: {
+            input: function ($event) {
+              if ($event.target.composing) {
+                return
+              }
+              _vm.name = $event.target.value
+            },
+          },
+        }),
       ]),
       _vm._v(" "),
-      _c("button", { staticClass: "btn btn-secondary btn-block mb-4" }, [
-        _vm._v("Szukaj"),
+      _c("div", { staticClass: "form-group col-md-6 mt-3" }, [
+        _c("input", {
+          directives: [
+            {
+              name: "model",
+              rawName: "v-model",
+              value: _vm.lastname,
+              expression: "lastname",
+            },
+          ],
+          staticClass: "form-control form-control-sm",
+          attrs: { type: "text", name: "lastname", placeholder: "Nazwisko..." },
+          domProps: { value: _vm.lastname },
+          on: {
+            input: function ($event) {
+              if ($event.target.composing) {
+                return
+              }
+              _vm.lastname = $event.target.value
+            },
+          },
+        }),
       ]),
-    ])
-  },
-]
+      _vm._v(" "),
+      _c("div", { staticClass: "form-group col-md-6 mt-3" }, [
+        _c("input", {
+          directives: [
+            {
+              name: "model",
+              rawName: "v-model",
+              value: _vm.pesel,
+              expression: "pesel",
+            },
+          ],
+          staticClass: "form-control form-control-sm",
+          attrs: { type: "text", name: "pesel", placeholder: "Pesel..." },
+          domProps: { value: _vm.pesel },
+          on: {
+            input: function ($event) {
+              if ($event.target.composing) {
+                return
+              }
+              _vm.pesel = $event.target.value
+            },
+          },
+        }),
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "form-group col-md-6 mt-3" }, [
+        _c("input", {
+          directives: [
+            {
+              name: "model",
+              rawName: "v-model",
+              value: _vm.birthday,
+              expression: "birthday",
+            },
+          ],
+          staticClass: "form-control form-control-sm",
+          attrs: {
+            type: "text",
+            name: "birthday",
+            placeholder: "Data urodzenia...",
+          },
+          domProps: { value: _vm.birthday },
+          on: {
+            input: function ($event) {
+              if ($event.target.composing) {
+                return
+              }
+              _vm.birthday = $event.target.value
+            },
+          },
+        }),
+      ]),
+    ]),
+    _vm._v(" "),
+    _c("button", { staticClass: "btn btn-secondary btn-block mb-4" }, [
+      _vm._v("Szukaj"),
+    ]),
+  ])
+}
+var staticRenderFns = []
 render._withStripped = true
 
 
--- resources/js/vue/components/Search.vue
+++ resources/js/vue/components/Search.vue
@@ -3,18 +3,43 @@
         <h5 class="text-uppercase text-secondary font-weight-bolder mt-3">Wyszukiwarka:</h5>
         <div class="form-row mb-4">
             <div class="form-group col-md-6 mt-3">
-                <input type="text" name="from" class="form-control form-control-sm" placeholder="Imię...">
+                <input type="text" name="name" v-model="name" class="form-control form-control-sm" placeholder="Imię...">
             </div>
             <div class="form-group col-md-6 mt-3">
-                <input type="text" name="from" class="form-control form-control-sm" placeholder="Nazwisko...">
+                <input type="text" name="lastname" v-model="lastname" class="form-control form-control-sm" placeholder="Nazwisko...">
             </div>
             <div class="form-group col-md-6 mt-3">
-                <input type="text" name="from" class="form-control form-control-sm" placeholder="Pesel...">
+                <input type="text" name="pesel" v-model="pesel" class="form-control form-control-sm" placeholder="Pesel...">
             </div>
             <div class="form-group col-md-6 mt-3">
-                <input type="text" name="from" class="form-control form-control-sm" placeholder="Data urodzenia...">
+                <input type="text" name="birthday" v-model="birthday" class="form-control form-control-sm" placeholder="Data urodzenia...">
             </div>
         </div>
         <button class="btn btn-secondary btn-block mb-4">Szukaj</button>
     </div>
 </template>
+<script>
+
+
+export default {
+    components: {
+    },
+    data() {
+        return {
+            name: null,
+            lastname: null,
+            pesel: null, 
+            birthday: null
+        };
+    },
+    computed: {
+
+    },
+    methods: {
+    },
+    created() {
+
+    },
+    props: {},
+}
+</script>
