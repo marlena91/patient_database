91fdaff81974b3c565ab83e7f32c6443eae911e1
Marlena Żarczyńska <marlena.zarczynska@gmail.com>
2022-04-20 12:18:31 +0200

    Added method of creating a pesel matching with the birthdate

--- database/factories/PatientFactory.php
+++ database/factories/PatientFactory.php
@@ -27,10 +27,29 @@ class PatientFactory extends Factory
     public function definition()
     {
         return [
-            'pesel' => $this->faker->numerify('###########'),
             'name' => $this->faker->firstName($gender = null),
             'lastname' => $this->faker->lastName,
-            'birthday' => $this->faker->dateTimeBetween($startDate = '-80 years', $endDate = 'now', $timezone = null),
+            'birthday' => $this->faker->dateTimeBetween($startDate = '-80 years', $endDate = 'now'),
+            'pesel' => function (array $model) {
+                $century = substr($model['birthday']->format('Y'), -4, 2);
+                $justYear = substr($model['birthday']->format('Y'), -2);
+                $justMonth = $model['birthday']->format('m');
+                $justDay = $model['birthday']->format('d');
+                if ($century == 18) {
+                    $justMonth += 80;
+                } else if ($century == 20) {
+                    $justMonth += 20;
+                } else if ($century == 21) {
+                    $justMonth += 40;
+                } else if ($century == 22) {
+                    $justMonth += 60;
+                }
+                $firstPart = $justYear . $justMonth . $justDay;
+
+                $secondPart = $this->faker->numerify('#####');
+
+                return $firstPart . $secondPart;
+            }
         ];
     }
 }
